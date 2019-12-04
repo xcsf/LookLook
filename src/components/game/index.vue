989fc840f294e49e3326738d31c3e1de0e4b363d
yinchengyu <jiaowofeizi@126.com>
2019-12-04 20:19:26 +0800

    add catch-the-cat

--- src/components/game/index.vue
+++ src/components/game/index.vue
@@ -1,13 +1,36 @@
 <template>
-  <div class="wrapper"></div>
+  <div class="wrapper">
+    <div id="catch-the-cat"></div>
+  </div>
 </template>
 <script>
 export default {
   name: "game",
   data() {
-    return {};
+    return {
+      oScript3: document.createElement("script")
+    };
   },
-  methods: {}
+  methods: {},
+  created() {},
+  mounted() {
+    this.oScript3.type = "text/javascript";
+    this.oScript3.innerText = `
+        new CatchTheCatGame({
+            w: 11,
+            h: 11,
+            r: 20,
+            backgroundColor: 0xeeeeee,
+            parent: "catch-the-cat",
+            statusBarAlign: "center",
+            credit: "xcsf"
+          });
+          `;
+    document.body.appendChild(this.oScript3);
+  },
+  destroyed() {
+    document.body.removeChild(this.oScript3);
+  }
 };
 </script>
 <style lang="scss" scoped>
